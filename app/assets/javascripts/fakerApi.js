bbc15af5e7d409e48215deb894452d5e875fd88c
David Lee <ddlee.907@gmail.com>
2016-01-29 03:22:33 -0500

    remove console.logs

--- app/assets/javascripts/fakerApi.js
+++ app/assets/javascripts/fakerApi.js
@@ -25,34 +25,3 @@ api.factory('fakerApi', ['$http', function($http){
   return fakerInfo;
 
 }]);
-
-// var fakerName = $http.get('http://faker.hook.io?property=name.findName&locale=de').then(function(response){
-//     console.log(response.data);
-//     return response.data;
-//   });
-
-//   var fakerUsername = $http.get('http://faker.hook.io?property=internet.userName&locale=de').then(function(response){
-//     console.log(response.data);
-//     return response.data;
-//   });
-
-//   var fakerImage = $http.get('http://faker.hook.io?property=image.people&locale=de').then(function(response){
-//     console.log(response.data);
-//     return response.data;
-//   });
-
-// getName.then(function(response){
-//       console.log(response.data);
-//       fakerInfo.name = response.data;
-//     });
-
-//     getUsername.then(function(response){
-//       var username = response.data;
-//       console.log(username);
-//       fakerInfo.username = "@"+username;
-//     });
-
-//     getImage.then(function(response){
-//       console.log(response.data);
-//       fakerInfo.image = response.data;
-//     });
--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -4,9 +4,7 @@ app.controller('UsersController', ['$scope', '$http', 'fakerApi', function($scop
 
   $http.get('/api/users').then(function(response){
     var data = response.data;
-    console.log(data);
     $scope.users = data.users;
-    console.log($scope.users);
   });
 
   $scope.users = [];
@@ -16,25 +14,18 @@ app.controller('UsersController', ['$scope', '$http', 'fakerApi', function($scop
   $scope.createTweet = function(){
 
     fakerApi.getName().then(function(response){
-      console.log(response);
       $scope.newUser.name = JSON.parse(response.data);
-      console.log($scope.newUser.name);
 
       fakerApi.getUsername().then(function(response){
         $scope.newUser.username = "@" + JSON.parse(response.data);
-        console.log($scope.newUser.username);
 
         fakerApi.getImage().then(function(response){
           $scope.newUser.image = JSON.parse(response.data);
-          console.log($scope.newUser.image);
 
           $http.post('/api/users', {user: $scope.newUser}).then(function(response){
             var data = response.data;
-            console.log(data);
 
             $scope.users.push({name: data.name, image: data.image, tweet: data.tweet, username: data.username});
-
-            console.log($scope.users);
 
           });
 
@@ -44,49 +35,8 @@ app.controller('UsersController', ['$scope', '$http', 'fakerApi', function($scop
 
     });
 
-
-
   };
-  console.log($scope.newUser);
-    return $scope.newUser;
-  console.log($scope.newUser);
+
+  return $scope.newUser;
 
 }]);
-
-
-
-
-      // $http.get('http://faker.hook.io?property=name.findName&locale=de').then(function(response){
-      //   $scope.newUser.name = response.data;
-      //   console.log($scope.newUser.name);
-
-      //   $http.get('http://faker.hook.io?property=internet.userName&locale=de').then(function(response){
-      //     $scope.newUser.image = response.data;
-      //     console.log($scope.newUser.image);
-
-      //     $http.get('http://faker.hook.io?property=image.people&locale=de').then(function(response){
-      //       $scope.newUser.username = response.data;
-      //       console.log($scope.newUser.username);
-
-      //       $http.post('/api/users', {user: $scope.newUser}).then(function(response){
-
-      //         console.log(response);
-
-      //         var data = response.data;
-
-      //         console.log(data.name);
-
-      //         $scope.users.push(data);
-
-      //         console.log($scope.users);
-      //       });
-
-      //     });
-
-      // //   });
-      //   console.log($scope.newUser);
-
-
-// http://faker.hook.io?property=name.findName&locale=de
-// http://faker.hook.io?property=internet.userName&locale=de
-// http://faker.hook.io?property=image.people&locale=de
