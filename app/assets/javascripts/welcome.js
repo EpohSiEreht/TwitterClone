8786fafec06839440f24416947f89a70c66849ed
David Lee <ddlee.907@gmail.com>
2016-02-05 20:31:48 -0500

    comment out created at time

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,16 +1,16 @@
 var app = angular.module('UserTweet', ['fakerApiFactory']);
 
-app.service('myDateFormat', function () { /* ... */ });
+// app.service('myDateFormat', function () { /* ... */ });
 
-app.filter('myDateFormat', function myDateFormat($filter){
-  return function(text){
-    var newDate = new Date(moment(text).format('YYYY-MM-DD hh:mm:ss a'));
-    console.log(newDate);
-    var fromNow = moment(newDate).fromNow();
-    console.log(fromNow);
-    return $filter('date')(fromNow);
-  }
-});
+// app.filter('myDateFormat', function myDateFormat($filter){
+//   return function(text){
+//     var newDate = new Date(moment(text).format('YYYY-MM-DD hh:mm:ss a'));
+//     console.log(newDate);
+//     var fromNow = moment(newDate).fromNow();
+//     console.log(fromNow);
+//     return $filter('date')(fromNow);
+//   }
+// });
 
 app.controller('UsersController', ['$scope', '$http', 'fakerApi', function($scope, $http, fakerApi){
 
